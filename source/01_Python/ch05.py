801b89f17e6576d7becf3fead39c0fcc79ee6103
Leeh949 <dlgmltjd3353@gmail.com>
2025-09-23 18:00:02 +0900

    Python_함수

---
+++ source/01_Python/ch05.py
@@ -0,0 +1,20 @@
+# ch05.py (ch05 모듈) 자동완성핫키: Ctrl_space
+# ctrl + shift + p => select interpreter을 이용해서 인터프리터(base) 선택
+# 실행: cmd 터미널(Ctrl + J )에서 python ch05.py
+# python ch05.py
+
+
+
+
+def my_hello(cnt): #cnt번 반복
+    for i in range(cnt):
+        print('hello, python',end='\t')
+        print('hello, World')
+
+
+if __name__ == '__main__':
+    my_hello(2)
+
+
+
+
